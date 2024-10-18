b954ecc9e405a0a5b4e60861af1a657973556589
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-10-18 08:56:02 +0700

    first commit

---
+++ app/src/main/java/com/app/gatebox/component/MainWebView.kt
@@ -0,0 +1,19 @@
+package com.app.gatebox.component
+
+import android.webkit.WebView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun MainWebView() {
+  val context = LocalContext.current
+  val webView = remember { WebView(context) }
+
+  AndroidView(
+    factory = {
+      webView
+    }
+  )
+}
